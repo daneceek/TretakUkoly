01d8d5880adbd2731b582f090caf9be47b774fcf
Danny Kročil <dannykrocil@seznam.cz>
2023-10-18 21:15:05 +0200

    init

---
+++ cyklus for.py
@@ -0,0 +1,26 @@
+# friends = ["Petr", "Pavel", "Tom", "Katka", "Klára", "Jiří", "Zdeněk"]
+# print(friends[-1]) #-1 da posledni jinak od nuly 
+# print(friends[1:]) # od Pavla dál
+# print(friends[1:4]) #po Katku ; 4 uz tam ale neni
+# friends[3] = "Lucka"
+# print(friends[3])
+# print (friends)
+
+stastna_cisla = [3,4,5,8,1,12,41,32]
+friends = ["Petr", "Pavel", "Tom", "Katka", "Klára", "Jiří", "Zdeněk"]
+#friends.append("Lukáš")
+#friends.extend(stastna_cisla)
+# friends.insert(1, "Marcel") #dá marcela na druhe misto v seznamu
+#friends.remove(friends[1])
+#del friends[0:3]
+#friends.clear()
+#friends.pop() #defaultně odstraní posledni vec [-1]
+#print(friends.count("e")) #pocita 
+#friends.sort() #abecedni poradi i s cisly 
+#friends.reverse()#otočí pořadí
+# friends2 = friends.copy()
+# print(friends2) #kopie seznamu
+#print(friends.index("Petr"))
+print(friends)
+
+
