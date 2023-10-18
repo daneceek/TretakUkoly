522f952732bc5c22f121ecddbdee80a10ecd684d
Danny <139589637+daneceek@users.noreply.github.com>
2023-10-18 13:31:33 +0200

    Delete generator.py
    smazano

--- generator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def random_number_generator(count, min, max):
-    result = []
-    for _ in range(count):
-        result.append(random.randint(min, max))
-        
-    return result
