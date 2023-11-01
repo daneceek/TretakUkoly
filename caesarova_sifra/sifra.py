a01ad3011da0cb08ba7587763a3b6295cb051c05
Danny Kročil <dannykrocil@seznam.cz>
2023-11-01 14:10:11 +0100

    sifra

---
+++ caesarova_sifra/sifra.py
@@ -0,0 +1,17 @@
+import unicodedata
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+
+def string_shift(text:str, key:int):
+    text = text.upper()
+    text = unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode("ascii").upper() # odstranění znaků s háčkem atd. 
+    result = ""
+    for char in text:
+        if char >= "A" and char <= "Z":
+            position = ord(char) - 65 
+            new_position = (position + key) % 26
+            result += chr(new_position + 65) 
+        else:
+            continue    
+    return result
+print(string_shift("Ahoj", 3)) 
